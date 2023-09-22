d0f9e4bc5446754f19a656126f63500d21739b5c
Jun830 <90589388+jun830@users.noreply.github.com>
2023-09-22 22:00:49 +0900

    0922_scale

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
 import tkinter as tk
-from  tkinter import ttk
 
+# メインウィンドウ
 root = tk.Tk()
-root.title("はじめてのtkinter")
-root.geometry("200x100")
+root.title("スケール")
 
-label = ttk.Label(root, text = "Hello Python").pack()
+# スケールの値を格納する
+red = tk.IntVar()
+red.set(0)
+blue = tk.IntVar()
+blue.set(0)
+green = tk.IntVar()
+green.set(0)
 
-entry = ttk.Entry(root).pack()
+# ボタンの背景色を変更
+def change_color(_):
+    color = '#{:02x}{:02x}{:02x}'.format(red.get(), green.get(), blue.get())
+    button.configure(bg = color)
 
-button = ttk.Button(root, text = "click").pack()
+# ボタン
+button = tk.Button(root, text = 'button', bg = '#000')
+button.pack(fill = 'both')
 
+# スケール
+s1 = tk.Scale(root, label = 'red', orient = 'h',
+              from_ = 0, to = 255, variable = red,
+              command = change_color)
+
+s2 = tk.Scale(root, label = 'blue', orient = 'h',
+              from_ = 0, to = 255, variable = blue,
+              command = change_color)
+
+s3 = tk.Scale(root, label = 'green', orient = 'h',
+              from_ = 0, to = 255, variable = green,
+              command = change_color)
+
+# ウィジェットの配置
+s1.pack(fill = 'both')
+s2.pack(fill = 'both')
+s3.pack(fill = 'both')
+
+# メインループ
 root.mainloop()
